1d38be34dffa9ee721e83719d2a3b48b03e05110
gfike19 <gfike19@gmail.com>
2025-07-28 15:00:01 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CardShuffler.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
